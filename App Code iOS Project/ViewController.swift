4311b7020be141b0fd0df6063f98a2be6e921861
elye <elye.project@gmail.com>
2020-02-19 00:36:32 +1100

    Add SnapKit and content to ViewController

--- App Code iOS Project/ViewController.swift
+++ App Code iOS Project/ViewController.swift
@@ -1,10 +1,19 @@
 import UIKit
-
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+        let textLabel = UILabel()
+        textLabel.text = "Hello World!"
+        textLabel.textAlignment = .center
+        view.addSubview(textLabel)
+        textLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.top.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
     }
 }
