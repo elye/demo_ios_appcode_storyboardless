2f9ef33c3adc55389225b96442053302c8e6983d
elye <elye.project@gmail.com>
2020-02-19 00:09:11 +1100

    Control and attache the ViewController to the rootview, and make the view white.

--- App Code iOS Project/AppDelegate.swift
+++ App Code iOS Project/AppDelegate.swift
@@ -4,8 +4,14 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    internal var window: UIWindow? = nil
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
-    return true
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = ViewController()
+        viewController.view.backgroundColor = .white
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+        return true
     }
 }
